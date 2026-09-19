9c27aaeaa152678fd3eff3049a43777bb887b989
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2026-09-19 19:00:29 +0300

    Update _kiss_fft_guts.h
    Fixed sincosf() path.

--- inc/_kiss_fft_guts.h
+++ inc/_kiss_fft_guts.h
@@ -146,7 +146,8 @@ struct kiss_fft_state{
 #endif
 
 
-#if 0//(!defined(FIXED_POINT) || !defined(USE_SIMD)) && HAS_SINCOSF
+#if !defined(FIXED_POINT) && !defined(USE_SIMD) && HAS_SINCOSF
+#  pragma message "kf_cexp: using sincosf (fast path)"
 #  define  kf_cexp(x,phase) \
      do{ \
        sincosf(phase, &(x)->i, &(x)->r);\
